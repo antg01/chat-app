13ff41da4b50071a21ebb7f0f497109572c5d42f
Antoine <ant.g@hotmail.be>
2020-10-14 12:05:21 +0200

    status

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Contact from './components/Contact.js';
+
+const user = {
+    avatar: 'https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    name: "Clifton Tucker",
+    mail: "clifton.tucker@example.com",
+    bday: "11/2/1985",
+    address: "9324 Spring St",
+    phoneNumber: "(220)-536-0019",
+    password: "harder",
+    isOnline: true,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact {...user}/>
+
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Contact.css';
+
+
+class Contact extends React.Component {
+    render() {
+        const {avatar, name, isOnline} = this.props;
+      return (
+        <div className="Contact">
+                <img className="avatar" src={avatar} alt="user avatar"/>
+            <div>
+                <h4 className="name">
+                    {name}
+                </h4>            
+                <div className="status">    
+                    {isOnline? <p className="status-online"></p> : <p className="status-offline"></p>}
+                    {isOnline? <p className="status-text">Online</p> : <p className="status-text">Offline</p>}
+                </div> 
+            </div>
+        </div>
+       )
+    }
+ }
+
+export default Contact;
